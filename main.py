d7d9f1c6dc765af877d433608fbf10b4e8f7d3c0
frederiknolte <me@frederiknolte.com>
2021-01-09 23:00:34 +0100

    rewrote main: - changed train to smaller function - moved kfold into separate function - moved model selection into separate function - added final training for best parameter set and evaluation to parameter search function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 
 from datasets import Dataset
 from arl import ARL
+from ipw import IPW
 from baseline_model import BaselineModel
 from metrics import Logger
 
@@ -40,25 +41,108 @@ def grid_search(args):
     best_mean_auc = 0
     
     for lr, bs in itertools.product(lr_list, batch_size_list):
-        args.prim_lr = lr
-        if args.model == 'ARL':
-            args.adv_lr = lr
-        
+        args.prim_lr, args.adv_lr = lr, lr
         args.batch_size = bs
-        mean_auc = train(args, version)
+        mean_auc = run_folds(args, version+'_folds')
         if mean_auc > best_mean_auc:
             best_mean_auc = mean_auc
             best_bs = bs
             best_lr = lr
-    
+
+    # Test model
+    train_dataset = Dataset(args.dataset, disable_warnings=args.disable_warnings)
+    test_dataset = Dataset(args.dataset, test=True, disable_warnings=args.disable_warnings)
+    args.prim_lr, args.adv_lr, args.batch_size = best_lr, best_lr, best_bs
+    train(args, train_dataset=train_dataset, test_dataset=test_dataset, version=version + '_test')
+
+    # save parameters
     best_params = {'learning_rate': best_lr, 'batch_size': best_bs}
     # write best params to file
-    with open('best_params.json', 'w') as f:
+    with open(os.path.join(args.log_dir, args.dataset, args.model, f'version_{version}_test', 'best_params.json'), 'w') as f:
         json.dump(best_params, f)
-    
+
     print(f'Best hyperparameters: {best_params}')
+
+
+def get_model(args, dataset):
+    """
+    TODO
+    :param args: TODO
+    :param dataset: TODO
+    :return: TODO
+    """
+    if args.model == 'ARL':
+        model = ARL(num_features=dataset.dimensionality,
+                    pretrain_steps=args.pretrain_steps,
+                    prim_hidden=args.prim_hidden,
+                    adv_hidden=args.adv_hidden,
+                    prim_lr=args.prim_lr,
+                    adv_lr=args.adv_lr,
+                    optimizer=OPT_BY_NAME[args.opt],
+                    opt_kwargs={})
+
+    elif args.model == 'DRO':
+        raise NotImplementedError
+
+    elif args.model == 'IPW':
+        model = IPW(num_features=dataset.dimensionality,
+                    hidden_units=args.prim_hidden,
+                    lr=args.prim_lr,
+                    optimizer=OPT_BY_NAME[args.opt],
+                    group_probs=dataset.group_probs,
+                    opt_kwargs={})
+        args.pretrain_steps = 0  # NO PRETRAINING
+
+    elif args.model == 'baseline':
+        model = BaselineModel(num_features=dataset.dimensionality,
+                              hidden_units=args.prim_hidden,
+                              lr=args.prim_lr,
+                              optimizer=OPT_BY_NAME[args.opt],
+                              opt_kwargs={})
+        args.pretrain_steps = 0  # NO PRETRAINING
+
+    return model
+
     
-def train(args, version=None):
+def run_folds(args, version=None):
+    """
+    Function to train a model
+    :param args: object from the argument parser
+    :version: used to group runs from a single grid search into the same directory
+    :return: Results from testing the model
+    """
+
+    # Create datasets
+    dummy_train_dataset = Dataset(args.dataset)
+
+    # perform n-fold crossvalidation
+    kf = KFold(n_splits=args.num_folds)
+    fold_nbr = 0
+    aucs = []
+    for train_idcs, val_idcs in kf.split(dummy_train_dataset):
+        fold_nbr += 1
+
+        # create datasets for fold
+        train_dataset = Dataset(args.dataset, idcs=train_idcs, disable_warnings=args.disable_warnings)
+        val_dataset = Dataset(args.dataset, idcs=val_idcs, disable_warnings=args.disable_warnings)
+
+        print(train_dataset)
+        print(val_dataset)
+
+        # train model
+        model = train(args, train_dataset=train_dataset, val_dataset=val_dataset, version=version, fold_nbr=fold_nbr)
+
+        # Evaluate on val set to get an estimate of performance
+        scores = torch.sigmoid(model(val_dataset.features))
+        aucs.append(auroc(scores, val_dataset.labels).item())
+    
+    mean_auc = np.mean(aucs)
+    print(f'mean val auc: {mean_auc}')
+
+    return mean_auc
+
+
+def train(args, train_dataset=None, val_dataset=None, test_dataset=None, version=None, fold_nbr=None):
     """
     Function to train a model
     :param args: object from the argument parser
@@ -72,106 +156,55 @@ def train(args, version=None):
     if version is None:
         version = str(int(time()))
 
-    # Seed for reproducability
-    pl.seed_everything(args.seed)
 
-    # Create datasets
-    dummy_train_dataset = Dataset(args.dataset)
-
-    test_dataset = Dataset(args.dataset, test=True)
-
-    test_loader = DataLoader(test_dataset,
-                             batch_size=args.batch_size,
-                             shuffle=False,
-                             num_workers=args.num_workers)
-
-    # set up callbacks which stay constant
-    test_callback = Logger(test_dataset, 'test')
-    
-    # perform n-fold crossvalidation
-    kf = KFold(n_splits=args.num_folds)
-    fold_nbr = 0
-    aucs = []
-    for train_idcs, val_idcs in kf.split(dummy_train_dataset):
-        fold_nbr += 1
-
-        # create fold datasets, loaders and callbacks
-        train_dataset = Dataset(args.dataset, idcs=train_idcs, disable_warnings=args.disable_warnings)
-        val_dataset = Dataset(args.dataset, idcs=val_idcs, disable_warnings=args.disable_warnings)
-
-        train_loader = DataLoader(train_dataset,
+    # create fold loaders and callbacks
+    train_loader = DataLoader(train_dataset,
                               batch_size=args.batch_size,
                               shuffle=True,
                               num_workers=args.num_workers)
 
-        validation_loader = DataLoader(val_dataset,
-                              batch_size=args.batch_size,
-                              shuffle=True,
-                              num_workers=args.num_workers)
-        
-        train_callback = Logger(train_dataset, 'training')
-        val_callback = Logger(val_dataset, 'validation')
-        early_stop_callback = EarlyStopping(
+    callbacks = [Logger(train_dataset, 'training')]
+
+    if val_dataset is not None:
+        callbacks.append(Logger(val_dataset, 'validation'))
+        callbacks.append(EarlyStopping(
             monitor='validation/micro_avg_auc',
             min_delta=0.00,
             patience=3,
             verbose=True,
             mode='max'
-        )
-    
+        ))
 
-        # Select model and instantiate
-        if args.model == 'ARL':
-            model = ARL(num_features=train_dataset.dimensionality,
-                        pretrain_steps=args.pretrain_steps,
-                        prim_hidden=args.prim_hidden,
-                        adv_hidden=args.adv_hidden,
-                        prim_lr=args.prim_lr,
-                        adv_lr=args.adv_lr,
-                        optimizer=OPT_BY_NAME[args.opt],
-                        opt_kwargs={})
+    if test_dataset is not None:
+        callbacks.append(Logger(test_dataset, 'test'))
 
-        elif args.model == 'DRO':
-            raise NotImplementedError
-        
-        elif args.model == 'baseline':
-            model = BaselineModel(num_features=train_dataset.dimensionality,
-                                hidden_units=args.prim_hidden,
-                                lr=args.prim_lr,
-                                optimizer=OPT_BY_NAME[args.opt],
-                                opt_kwargs={})
-            args.pretrain_steps = 0 # NO PRETRAINING
+    # Select model and instantiate
+    model = get_model(args, train_dataset)
 
-        # create logger
-        logger = TensorBoardLogger(save_dir='./', name=logdir, version=f'version_{version}/lr_{args.prim_lr}_bs_{args.batch_size}/fold_{fold_nbr}')
+    # create logger
+    logger = TensorBoardLogger(
+        save_dir='./',
+        name=logdir,
+        version=f'version_{version}/lr_{args.prim_lr}_bs_{args.batch_size}'
+                + (f'/fold_{fold_nbr}' if fold_nbr is not None else '')
+    )
 
-        #raise ValueError
-        # Create a PyTorch Lightning trainer
-        trainer = pl.Trainer(logger=logger,
-                            checkpoint_callback=ModelCheckpoint(save_weights_only=True, dirpath=logger.log_dir),
-                            gpus=1 if torch.cuda.is_available() else 0,
-                            max_steps=args.train_steps+args.pretrain_steps,
-                            callbacks=[train_callback, val_callback, early_stop_callback], # test callback?
-                            progress_bar_refresh_rate=1 if args.p_bar else 0
-                            #fast_dev_run=True # FOR DEBUGGING, SET TO FALSE FOR REAL TRAINING
-                            )
+    # raise ValueError
+    # Create a PyTorch Lightning trainer
+    trainer = pl.Trainer(logger=logger,
+                         checkpoint_callback=ModelCheckpoint(save_weights_only=True, dirpath=logger.log_dir),
+                         gpus=1 if torch.cuda.is_available() else 0,
+                         max_steps=args.train_steps + args.pretrain_steps,
+                         callbacks=callbacks,
+                         progress_bar_refresh_rate=1 if args.p_bar else 0
+                         # fast_dev_run=True # FOR DEBUGGING, SET TO FALSE FOR REAL TRAINING
+                         )
 
-        # Training
-        trainer.fit(model, train_loader)
+    # Training
+    trainer.fit(model, train_loader)
 
-        # Evaluate on val set to get an estimate of performance
-        scores = torch.sigmoid(model(val_dataset.features))
-        aucs.append(auroc(scores, val_dataset.labels).item())
-    
-    mean_auc = np.mean(aucs)
-    print(f'mean val auc: {mean_auc}')
+    return model
 
-    # Testing
-    #model = model.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
-    #test_result = trainer.test(model, test_dataloaders=test_loader, verbose=True)
-
-    #return mean_auc
-    return mean_auc
 
 if __name__ == "__main__":
     
@@ -179,7 +212,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model settings
-    parser.add_argument('--model', choices=['baseline', 'ARL', 'DRO'], required=True)
+    parser.add_argument('--model', choices=['baseline', 'ARL', 'DRO', 'IPW'], required=True)
     parser.add_argument('--prim_hidden', default=[64, 32], help='Number of hidden units in primary network')
     parser.add_argument('--adv_hidden', default=[32], help='Number of hidden units in adversarial network')
 
@@ -203,6 +236,9 @@ if __name__ == "__main__":
     parser.add_argument('--disable_warnings', default=False, type=bool, help='Whether to disable warnings about mean and std in the dataset')
 
     args = parser.parse_args()
+
+    # Seed for reproducability
+    pl.seed_everything(args.seed)
 
     if args.grid_search:
         grid_search(args)
