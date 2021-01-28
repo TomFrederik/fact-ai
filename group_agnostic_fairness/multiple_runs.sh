e77e460dff96a6cbf9c5be8299a13c19dd09829c
Erik Jenner <erik.jenner99@gmail.com>
2021-01-28 18:39:17 +0100

    Add scripts for averaging TF results

---
+++ group_agnostic_fairness/multiple_runs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+mkdir -p results
+cd ..
+
+lr=0.1
+bs=128
+dataset="$1"
+model="$2"
+steps=1000
+
+echo "Dataset and model:"
+echo "$dataset $model"
+
+run () {
+    echo "Starting run with seed $1"
+    mkdir -p "group_agnostic_fairness/results/${model}_${dataset}_${lr}_${bs}_${steps}"
+    # the trainer outputs lots of deprecation and other warnings,
+    # hide everything to not clutter up this scripts output
+    python 2>/dev/null \
+	   -m group_agnostic_fairness.main_trainer \
+	   --train_steps "$steps" \
+	   --test_steps 100 \
+	   --primary_learning_rate "$lr" \
+	   --adversary_learning_rate "$lr" \
+	   --batch_size "$bs" \
+	   --dataset "$dataset" \
+	   --model_name "$model" \
+	   --seed "$1" \
+	   --output_file_path "group_agnostic_fairness/results/${model}_${dataset}/$1.json"
+    echo "Finished run with seed $1"
+}
+
+# Parallelize across N processes
+# See https://unix.stackexchange.com/a/436713
+N=4
+
+for seed in {1..10}; do
+    run "$seed" &
+
+    # allow to execute up to $N jobs in parallel
+    if [[ $(jobs -r -p | wc -l) -ge $N ]]; then
+        # now there are $N jobs already running, so wait here for any job
+        # to be finished so there is a place to start next one.
+        wait -n
+    fi
+
+done
+
+# no more jobs to be started but wait for pending jobs
+# (all need to be finished)
+wait
+
+cd -
+echo "Averaging results"
+python average_results.py "results/${model}_${dataset}"
+echo "Done."
